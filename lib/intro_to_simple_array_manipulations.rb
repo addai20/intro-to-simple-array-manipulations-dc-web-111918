3debc29c9e70592d65dd25543ea98852a5984bcd
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-15 20:55:14 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,22 +33,27 @@ def using_concat(arr1,arr2)
 end
 
 def using_insert(array, ele)
+  arr.insert(ele)
   
 end
 
 def using_uniq(array)
+  array.uniq
   
 end
 
-def using flatten
+def using_flatten(array)
+  array.flatten
   
 end
 
-def using_delete
+def using_delete(array, string)
+  array.delete(string)
   
 end
 
-def using_delete_at
+def using_delete_at(array, integer)
+  arra.delete(integer)
   
 end
 
